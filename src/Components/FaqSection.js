8275a9c337f97664402a0cc1b1928b58a2a7216f
Sheraz Jabbar <sherazjabbar999@gmail.com>
2021-09-28 00:43:36 -0700

    4. Faq Section

---
+++ src/Components/FaqSection.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat
+            explicabo
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat
+            explicabo
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat
+            explicabo
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat
+            explicabo
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 // Page Components
 import AboutSection from "../Components/AboutSection";
 import ServicesSection from "../Components/ServicesSection";
+import FaqSection from "../Components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   );
 };
