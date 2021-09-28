3e85ed82eeac9ce08510549719e4d12b9679e209
Sheraz Jabbar <sherazjabbar999@gmail.com>
2021-09-28 02:57:10 -0700

    5. Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyle from "./Components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
